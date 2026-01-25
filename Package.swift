5889d36a8b76838425d62a0f9a51a8f7fec34b3a
Shafee Rehman <shafee.rehman@adscale.co.nz>
2026-01-26 09:49:09 +1300

    Testing back to 10.3.1

--- Package.swift
+++ Package.swift
@@ -35,22 +35,22 @@ let package = Package(
         // ---- Remote binary XCFrameworks (fill URLs; checksums as provided) ----
         .binaryTarget(
             name: "YieldloveAdIntegration",
-            url: "https://github.com/mbrtargeting/stroeerSDK-iOS-spm/releases/download/10.3.2/YieldloveAdIntegration.xcframework.zip",
+            url: "https://github.com/mbrtargeting/stroeerSDK-iOS-spm/releases/download/10.3.1/YieldloveAdIntegration.xcframework.zip",
             checksum: "7375d5a07656108f7accc0a8890acbb61d523395dcdeee5aafcc0e2386badd86"
         ),
         .binaryTarget(
             name: "YieldloveAdIntegration_Consent",
-            url: "https://github.com/mbrtargeting/stroeerSDK-iOS-spm/releases/download/10.3.2/YieldloveAdIntegration_Consent.xcframework.zip",
+            url: "https://github.com/mbrtargeting/stroeerSDK-iOS-spm/releases/download/10.3.1/YieldloveAdIntegration_Consent.xcframework.zip",
             checksum: "3f9a577ec3a106170a6ec434f98b75b129ef8cd2d04551208b93f1c3719b997f"
         ),
         .binaryTarget(
             name: "YieldloveAdIntegration_Confiant",
-            url: "https://github.com/mbrtargeting/stroeerSDK-iOS-spm/releases/download/10.3.2/YieldloveAdIntegration_Confiant.xcframework.zip",
+            url: "https://github.com/mbrtargeting/stroeerSDK-iOS-spm/releases/download/10.3.1/YieldloveAdIntegration_Confiant.xcframework.zip",
             checksum: "095fcb275cc6903783626140b8b852d0c3f6d825d6c4de9a403cbfec10555a6d"
         ),
         .binaryTarget(
             name: "YieldloveAdIntegration_Gravite",
-            url: "https://github.com/mbrtargeting/stroeerSDK-iOS-spm/releases/download/10.3.2/YieldloveAdIntegration_Gravite.xcframework.zip",
+            url: "https://github.com/mbrtargeting/stroeerSDK-iOS-spm/releases/download/10.3.1/YieldloveAdIntegration_Gravite.xcframework.zip",
             checksum: "2e05ba7ed90801b6af10fa695b8583297a8faf8061bc2fb0d4cc34f9f592829b"
         ),
         .binaryTarget(
